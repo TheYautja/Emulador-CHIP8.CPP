fdf573ef403379bc9fb4c35065c2d2b0ebc975dd
TheYautja <am.96138669@gmail.com>
2025-08-06 22:05:07 -0300

    add file

---
+++ CHIP8.cpp
@@ -0,0 +1,25 @@
+// C:\msys64\ucrt64\bin\g++.exe -fdiagnostics-color=always -g "C:\Users\Asus\Desktop\C++\CHIP8\CHIP8.cpp" -o "C:\Users\Asus\Desktop\C++\CHIP8\CHIP8.exe"
+ 
+// "C:\Users\Asus\Desktop\C++\CHIP8\CHIP8.exe"
+ 
+#include <bitset>
+#include <iostream>
+
+class CPU {
+    public:
+        CPU(){
+
+            std::bitset<4096> memoria;
+            std::bitset<16> stack;
+            std::bitset<16> registradores;
+
+        }
+};
+
+int main(){
+    
+    CPU();
+    std::cout << "funcionando" << std::endl;
+
+    return 0;
+}
