d6e8ecec215f479a4d2532fc4a4bdedcd8a8c9fd
Salsicha <am.96138669@gmail.com>
2025-08-12 14:18:15 -0300

    Update CHIP8.cpp

--- CHIP8.cpp
+++ CHIP8.cpp
@@ -1,7 +1,3 @@
-// C:\msys64\ucrt64\bin\g++.exe -fdiagnostics-color=always -g "C:\Users\Asus\Desktop\C++\CHIP8\CHIP8.cpp" -o "C:\Users\Asus\Desktop\C++\CHIP8\CHIP8.exe"
- 
-// "C:\Users\Asus\Desktop\C++\CHIP8\CHIP8.exe"
- 
 #include <bitset>
 #include <iostream>
 
